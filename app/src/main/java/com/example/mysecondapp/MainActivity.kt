840d291e7a410ce1e1cbb12e033cb257b7b22085
三浦勇 <c01150788f@edu.teu.ac.jp>
2019-04-24 04:54:35 +0900

    お試しMVUarchitecture

--- app/src/main/java/com/example/mysecondapp/MainActivity.kt
+++ app/src/main/java/com/example/mysecondapp/MainActivity.kt
@@ -2,12 +2,60 @@ package com.example.mysecondapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+typealias  Model = Int
+
+sealed class Msg {
+    object Increment : Msg()
+    object Decrement : Msg()
+}
+
+typealias update = (Msg) -> (Model) -> Model
 
 class MainActivity : AppCompatActivity() {
-
+    //VIEW
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val messageView: TextView = findViewById(R.id.message_view)
+        messageView.text = init().toString()
+
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val msg: Msg = Msg.Increment
+            messageView.text = update()(msg)(Integer.parseInt(messageView.text.toString())).toString()
+        }
+
+
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val msg: Msg = Msg.Decrement
+            messageView.text = update()(msg)(Integer.parseInt(messageView.text.toString())).toString()
+        }
+
     }
-    
+
+    //MODEL
+    fun init(): Model = 0
+
+
+    //UPDATE
+    fun update(): update {
+        return { msg ->
+            when (msg) {
+                is Msg.Increment -> { model ->
+                    model + 1
+                }
+                is Msg.Decrement -> { model ->
+                    model - 1
+                }
+            }
+        }
+    }
 }
